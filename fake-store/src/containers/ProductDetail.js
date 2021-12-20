adbd2b9d89fd81b1e9162a8541b6750218de52f0
cukier91 <konarowski.bartosz@gmail.com>
2021-12-20 14:38:30 +0100

    Sklep jest w pełni funkcjonalny

--- fake-store/src/containers/ProductDetail.js
+++ fake-store/src/containers/ProductDetail.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { selectedProduct } from "../redux/actions/productActions";
+import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
 
 export default function ProductDetail() {
-    return (
-        <div>
-            TEst
-        </div>
-    )
+	const product = useSelector((state) => state.product);
+	const { productId } = useParams();
+	const dispatch = useDispatch();
+    
+	
+	const fetchProductDetail = async () => {
+		const rensponse = await axios
+			.get(`https://fakestoreapi.com/products/${productId}`)
+			.catch((err) => {
+				console.log("Error", err);
+			});
+		dispatch(selectedProduct(rensponse));
+		console.log(rensponse);
+	};
+
+	useEffect(() => {
+		if (productId && productId !== "") fetchProductDetail();
+	}, [productId]);
+	return (
+		<div>
+			{Object.keys(product).length === 0 ? (
+				<div> loading </div>
+			) : (
+				<Card style={{height:'auto', maxWidth:'35rem', display:'block', marginLeft:'auto', marginRight:'auto', marginTop:'5rem'}}>
+					<Card.Img variant="top" src={product.data.image} />
+					<Card.Body>
+						<Card.Title>{product.data.title}</Card.Title>
+						<Card.Text>{product.data.description}</Card.Text>
+					</Card.Body>
+					<ListGroup className="list-group-flush">
+						<ListGroupItem>Category: {product.data.category}</ListGroupItem>
+						<ListGroupItem>Price: {product.data.price}</ListGroupItem>
+						<ListGroupItem>Rate: {product.data.rating.rate}</ListGroupItem>
+					</ListGroup>
+				</Card>
+			)}
+		</div>
+	);
 }
--- fake-store/src/redux/reducers/productReducer.js
+++ fake-store/src/redux/reducers/productReducer.js
@@ -1,16 +1,23 @@
-import { ActionTypes } from "../const/action-types"
+import { ActionTypes } from "../const/action-types";
 
 const initialState = {
-    products: []
-}
+	products: [],
+};
 
-export const productReducer = (state=initialState, {type, payload}) => {
+export const productReducer = (state = initialState, { type, payload }) => {
+	switch (type) {
+		case ActionTypes.SET_PRODUCTS:
+			return { ...state, products: payload };
+		default:
+			return state;
+	}
+};
 
-    switch (type) {
-        case ActionTypes.SET_PRODUCTS:
-            return {...state, products: payload}
-        default:
-            return state;
-    }
-
-}
+export const selectedProductReducer = (state = {}, { type, payload }) => {
+	switch (type) {
+		case ActionTypes.SELECTED_PRODUCT:
+			return { ...state, ...payload };
+		default:
+			return state;
+	}
+};
