1f1f3912492d89d8e4ea727529529391e2304523
cukier91 <konarowski.bartosz@gmail.com>
2022-01-09 11:41:12 +0100

    Zmiany 09.01.2022

--- fake-store/src/containers/ProductDetail.js
+++ fake-store/src/containers/ProductDetail.js
@@ -32,52 +32,46 @@ export default function ProductDetail() {
 	return (
 		<div>
 			{Object.keys(product).length === 0 ? (
-				<div style={{
-				
-					marginTop:'20%',
-					marginLeft:'auto',
-					marginRight:'auto',
-					display:'flex',
-					alignItems:'center',
-					justifyContent:'center'
-					
-				}}>
-					<Spinner
-						animation="grow"
-						variant="dark"
-							
-					/>
+				<div
+					style={{
+						marginTop: "20%",
+						marginLeft: "auto",
+						marginRight: "auto",
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "center",
+					}}
+				>
+					<Spinner animation="grow" variant="dark" />
 				</div>
 			) : (
-			
-					<Card
-						style={{
-							height: "auto",
-							maxWidth: "60em",
-							display: "block",
-							marginLeft: "auto",
-							marginRight:'auto',
-							marginTop: "2%",
-							marginBottom:'2%'
-						}}
-					>
-						<Card.Img variant="top" src={product.image} />
-					
-						<Card.Body>
-							<Card.Title>{product.title}</Card.Title>
-							<Card.Text>{product.description}</Card.Text>
-						</Card.Body>
-						<ListGroup className="list-group-flush">
-							<ListGroupItem>Category: {product.category}</ListGroupItem>
-							<ListGroupItem>
-								Rate: <b>{product.rating.rate}</b>
-							</ListGroupItem>
-							<Button>
-								Price: <b>{product.price} $</b>
-							</Button>
-						</ListGroup>
-					</Card>
-			
+				<Card
+					style={{
+						height: "auto",
+						maxWidth: "50%",
+						display: "block",
+						marginLeft: "auto",
+						marginRight: "auto",
+						marginTop: "2%",
+						marginBottom: "2%",
+					}}
+				>
+					<Card.Img variant="top" src={product.image} />
+
+					<Card.Body>
+						<Card.Title>{product.title}</Card.Title>
+						<Card.Text>{product.description}</Card.Text>
+					</Card.Body>
+					<ListGroup className="list-group-flush">
+						<ListGroupItem>Category: {product.category}</ListGroupItem>
+						<ListGroupItem>
+							Rate: <b>{product.rating.rate}</b>
+						</ListGroupItem>
+						<Button>
+							Price: <b>{product.price} $</b>
+						</Button>
+					</ListGroup>
+				</Card>
 			)}
 		</div>
 	);
